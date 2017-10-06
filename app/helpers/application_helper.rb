675f600ec589ad3af11a28537ee3b3e518482ca9
Quentin Bellanger <hello@quentin-bellanger.com>
2017-10-06 17:58:23 +0200

    Fix css error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
-  def mailto_url
-    'mailto:hello@quentin-bellanger.com'
-  end
-
   def author_url
     'http://quentin-bellanger.com'
   end
 
-  def share_url
+  def contact_url
+    'mailto:hello@quentin-bellanger.com'
+  end
+
+  def github_url
     'https://github.com/bellangerq/the-shorter-the-better'
   end
 end
